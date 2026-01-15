10c031bc218d18d61257da00f6ace1774ca46123
Will Squibb <109985186+squibbium-235@users.noreply.github.com>
2026-01-15 10:05:51 +0000

    updated dropdown width

--- ide/src/main.rs
+++ ide/src/main.rs
@@ -281,7 +281,7 @@ html, body {
   position: absolute;
   top: 30px;
   left: 0;
-  min-width: 250px;
+  min-width: 350px;
   background: #0c0f16;
   border: 1px solid var(--border);
   box-shadow: 0 8px 30px rgba(0,0,0,0.35);

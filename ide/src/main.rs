ef38878da3eb9aadd6d55a5f06892be50662ef33
Will Squibb <109985186+squibbium-235@users.noreply.github.com>
2026-02-04 11:43:12 +0000

    Update main.rs
    hopefully fixed windows being windows

--- ide/src/main.rs
+++ ide/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use base64::{engine::general_purpose::STANDARD, Engine as _};
 use dioxus::prelude::*;
 use rfd::AsyncFileDialog;
